56b071fde81142c50fa2254fe941671c6e84f50f
Sergey <sergejananev78@gmail.com>
2024-03-25 21:57:57 +0300

    Back::Admin: Регистрация модели

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Appeal
 
 # Register your models here.
+
+
+class AppealAdminPage(admin.ModelAdmin):
+    fieldsets = [
+        (
+            "Заявка",
+            {
+                "fields": ["name", "phone"],
+            },
+        ),
+        (None, {"fields": ["date_created", "date_updated"]}),
+        (None, {"fields": ["comment"]}),
+        ("Дополнительно", {"fields": ["notes"]}),
+    ]
+    readonly_fields = ("date_created", "date_updated")
+
+
+admin.site.register(Appeal, AppealAdminPage)
