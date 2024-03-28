7a81a0ea6112885cce43a3bc84ed3c6ca7a6f541
Sergey <sergejananev78@gmail.com>
2024-03-28 17:14:11 +0300

    Back::Admin экшены для экспорта дб

--- back/api/admin.py
+++ back/api/admin.py
@@ -8,81 +8,62 @@ import datetime
 
 
 class AppealAdmin(admin.ModelAdmin):
+    def __export_as(self, file_type, queryset):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = f"attachment; filename=Imported_data_{current_time}.{file_type}"
+
+        field_names = ["name", "phone", "date_created", "date_updated", "comment", "notes", "id"]
+        field_verbose_names = [
+            "Имя заказчика",
+            "Телефон заказчика",
+            "Дата обращения",
+            "Дата последнего обновления",
+            "Комментарий к обращению",
+            "Заметки по заказу",
+            "ID в базе",
+        ]
+
+        if file_type == "csv":
+            writer = csv.writer(response)
+
+            writer.writerow(field_verbose_names)
+
+            for obj in queryset:
+                result = [getattr(obj, field) for field in field_names]
+
+                result[2] = result[2].replace(tzinfo=None)
+                result[3] = result[3].replace(tzinfo=None)
+
+                writer.writerow(result)
+        else:
+            wb = Workbook()
+
+            ws = wb.active
+            ws.title = "Таблица Обращений"
+
+            ws.append(field_verbose_names)
+
+            for obj in queryset:
+                result = [getattr(obj, field) for field in field_names]
+
+                result[2] = result[2].replace(tzinfo=None)
+                result[3] = result[3].replace(tzinfo=None)
+
+                ws.append(result)
+
+            wb.save(response)
+
+        return response
+
     actions = ["export_as_xlsx", "export_as_csv"]
 
     def export_as_csv(self, request, queryset):
-        meta = self.model._meta
-
-        current_time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-
-        response = HttpResponse(content_type="text/csv")
-        response["Content-Disposition"] = f"attachment; filename=Imported_csv_data_{current_time_str}.csv".format(meta)
-
-        writer = csv.writer(response)
-
-        field_names = ["name", "phone", "date_created", "date_updated", "comment", "notes", "id"]
-        field_verbose_names = [
-            "Имя заказчика",
-            "Телефон заказчика",
-            "Дата обращения",
-            "Дата последнего обновления",
-            "Комментарий к обращению",
-            "Заметки по заказу",
-            "ID в базе",
-        ]
-
-        writer.writerow(field_verbose_names)
-
-        for obj in queryset:
-            result = [getattr(obj, field) for field in field_names]
-
-            result[2] = result[2].replace(tzinfo=None)
-            result[3] = result[3].replace(tzinfo=None)
-
-            writer.writerow(result)
-
-        return response
+        return self.__export_as("csv", queryset)
 
     def export_as_xlsx(self, request, queryset):
-        meta = self.model._meta
-
-        current_time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-
-        response = HttpResponse(content_type="text/csv")
-        response["Content-Disposition"] = f"attachment; filename=Imported_xslx_data_{current_time_str}.xlsx".format(
-            meta
-        )
-
-        wb = Workbook()
-
-        ws = wb.active
-        ws.title = "Таблица Обращений"
-
-        field_names = ["name", "phone", "date_created", "date_updated", "comment", "notes", "id"]
-
-        field_verbose_names = [
-            "Имя заказчика",
-            "Телефон заказчика",
-            "Дата обращения",
-            "Дата последнего обновления",
-            "Комментарий к обращению",
-            "Заметки по заказу",
-            "ID в базе",
-        ]
-
-        ws.append(field_verbose_names)
-
-        for obj in queryset:
-            result = [getattr(obj, field) for field in field_names]
-
-            result[2] = result[2].replace(tzinfo=None)
-            result[3] = result[3].replace(tzinfo=None)
-
-            ws.append(result)
-
-        wb.save(response)
-
-        return response
+        return self.__export_as("xlsx", queryset)
 
     export_as_csv.short_description = "Экспортировать в csv выбранные поля"
     export_as_xlsx.short_description = "Экспортировать в xlsx выбранные поля"
